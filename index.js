426d0eca380c2e9bb3bdd077b94397614227b527
Harsh Tiwari <harsh@kimo.ai>
2025-01-15 21:53:23 +0530

    Update changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import app from "./app.js";
-import { connectDB } from "./config/db.js";
+import app from "./src/app.js";
+import { connectDB } from "./src/config/db.js";
 
 const PORT = process.env.PORT || 8000;
 
